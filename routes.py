2044a5bcdfc941a345c84113ae1fab07a98f463d
pchiquini <pchiquini@utexas.edu>
2017-05-02 19:20:58 -0500

    working on log in

--- routes.py
+++ routes.py
@@ -10,9 +10,22 @@ db.init_app(app)
 def index():
   return render_template("index.html")
 
+def home():
+	return render_template("home.html")
+
 @app.route("/about")
 def about():
   return render_template("about.html")
+
+
+@app.route('/login', methods=["GET","POST"])
+def login():
+    if request.form['password'] == 'admin' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+        return redirect(url_for('home'))
+    else:
+    	flash('wrong password!')
+    return home()
 
 if __name__ == "__main__":
   app.run(debug=True)
